41eaecfc130a5f82ec8cdaf531b415594d87044b
MISHAL ALHAJRI <mishalalhajri@MISHALs-MacBook-Air.local>
2019-07-22 23:50:02 +0300

    UI Finished

---
+++ README.md
@@ -0,0 +1,1 @@
+#This repo for cloning instagram
---
+++ instagramclone/Controller/SignupVC.swift
@@ -0,0 +1,33 @@
+//
+//  SignupVC.swift
+//  instagramclone
+//
+//  Created by MISHAL ALHAJRI on 19/11/1440 AH.
+//  Copyright © 1440 Torch. All rights reserved.
+//
+
+import UIKit
+
+class SignupVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func alreadyHaveAccountPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
